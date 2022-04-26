bea2b11a265220a083c93d1e036c030cf7bfa6f0
perloignacio <perloignacio@gmail.com>
2022-04-26 08:55:58 -0300

    app

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { correccion } from './models/correcion.model';
+import { glifosato } from './models/glifosato.mode';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  glifo:glifosato=new glifosato();
+  corre:correccion=new correccion();
+  ingrsoglif:boolean=true;
   title = 'app';
+  ingrsogcorr:boolean=true;
+  totalesta:number=0;
+  totalako:number=0;
+  resultadofinal=false;
+  loading:boolean=true;
+  CalularGli(){
+    this.glifo.porinactivacion=this.glifo.volumen*this.glifo.dureza*this.glifo.villaseca;
+    this.glifo.valinactivacion=this.glifo.costo*(this.glifo.porinactivacion/100);
+    this.glifo.perdida=this.glifo.dosis*this.glifo.valinactivacion;
+    this.ingrsoglif=false;
+    this.CalcularCostos();
+  }
+
+  OtroGli(){
+    this.ingrsoglif=true;
+    this.glifo=new glifosato
+  }
+
+  CalularCorr(){
+    this.corre.canthec=Number.parseFloat((this.corre.volumen/this.corre.litros).toFixed(2));
+    this.corre.costohec=Number.parseFloat((this.corre.costoeco/this.corre.canthec).toFixed(2));
+    ;
+    this.ingrsogcorr=false;
+    this.CalcularCostos();
+  }
+
+  CalcularCostos(){
+    if(!this.ingrsogcorr && !this.ingrsoglif){
+      this.resultadofinal=true;
+      setTimeout(()=>{                           //<<<---using ()=> syntax
+        
+        
+        this.loading=false;
+        this.totalesta=Number.parseFloat((this.corre.hectareas*this.corre.cantidad*this.glifo.perdida).toFixed(2));
+        this.totalako=Number.parseFloat((this.corre.hectareas*this.corre.cantidad*this.corre.costohec).toFixed(2));
+      }, 3000);
+    }
+
+  }
+  OtroCorr(){
+    this.ingrsogcorr=true;
+    this.corre=new correccion
+  }
+
+  nuevo(){
+    this.OtroCorr();
+    this.OtroGli();
+    this.loading=true;
+    this.resultadofinal=false;
+  }
 }
---
+++ app/src/app/models/correcion.model.ts
@@ -0,0 +1,9 @@
+export class correccion{
+    volumen:number=0;
+    litros:number=0;
+    cantidad:number=0;
+    costoeco:number=0;
+    hectareas:number=0;
+    canthec:number=0;
+    costohec:number=0;
+}
