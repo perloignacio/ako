01eb5b9b40bc29ff46420751b1671306bb0c75e6
Fedu <federico.mateos4@gmail.com>
2022-05-16 20:19:17 -0300

    listado de productos

--- app/src/app/components/productos/productos.component.ts
+++ app/src/app/components/productos/productos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Productos } from 'src/app/models/productos.model';
+import productos from 'src/assets/json/productos.json';
 
 @Component({
   selector: 'app-productos',
@@ -8,7 +9,8 @@ import { Productos } from 'src/app/models/productos.model';
   styleUrls: ['./productos.component.scss']
 })
 export class ProductosComponent implements OnInit {
-  
+  Fertilizantes: Productos[] =productos.prods.filter(p=>p.Tipo=='fertilizantes');
+  Coadyuvantes: Productos[] =productos.prods.filter(p=>p.Tipo=='coadyuvantes');
   constructor() {
     
    }
